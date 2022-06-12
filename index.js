80841e0aa22ee76806ab40cf8b568a6a17b31ff0
Jonah Avillanoza <j.avillanoza@ufl.edu>
2022-06-12 19:07:26 -0400

    Add li items trying to work through line through

--- index.js
+++ index.js
@@ -1,10 +1,51 @@
 var form = document.querySelector('form')
 var addToDo = document.querySelector('add-todo')
 var input= document.querySelector('input')
+const add = document.querySelector('button')
+const toDoList = document.querySelector('#todo-list')
 
-form.onsubmit = function (e) {
-    e.preventDefault()
+// Add Button event
+form.addEventListener('submit', onSubmit);
+
+//Delete event
+toDoList.addEventListener('click' , removeItem)
+
+//add item 
+function onSubmit(e){
+    e.preventDefault();
     console.log(input.value)
-    // addToDo.input = this.input.value
-    // this.input.value = ""
+
+    if (input.value){
+        //Create new ul item
+        const li = document.createElement('li');
+
+        // Add a list item within ToDo 
+        for (let i = 0; i < toDoList.length; i++) {
+            const li = item[i];
+        }
+        
+        // Add a Class name
+        li.className = 'items'
+        console.log(li)
+
+        //add textnode with input value
+        li.appendChild(document.createTextNode(`${input.value}`))
+        
+        //Appending Child 
+        toDoList.appendChild(li);
+
+
+        input.value = '';
+    }
+    
+}
+
+// Remove item
+function removeItem(e){
+    console.log('clicked for strike through');
+    const strikeThrough = document.querySelector('items')
+    
+    
+    
+
 }
